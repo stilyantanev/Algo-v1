b5166888689d4ffa25c3f3c977b9e07e2c8d7c44
stilyantanev <stilyan.tanev@gmail.com>
2015-06-09 12:28:24 +0300

    Fix identation

--- Application/palindromes/Palindromes.java
+++ Application/palindromes/Palindromes.java
@@ -4,67 +4,67 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Palindromes {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String input = in.nextLine();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
 
-		ArrayList<String> parts = getAllPartsOfString(input);
-		ArrayList<String> allPalindromes = new ArrayList<>();
+        ArrayList<String> parts = getAllPartsOfString(input);
+        ArrayList<String> allPalindromes = new ArrayList<>();
 
-		for (int i = 0; i < parts.size(); i++) {
-			String part = parts.get(i);
+        for (int i = 0; i < parts.size(); i++) {
+            String part = parts.get(i);
 
-			if (checkIsPalindrome(part)) {
-				allPalindromes.add(part);
-			}
-		}
+            if (checkIsPalindrome(part)) {
+                allPalindromes.add(part);
+            }
+        }
 
-		if (allPalindromes.isEmpty()) {
-			printNone();
-		} 
-		else {
-			printAllPalindromes(allPalindromes);
-		}
+        if (allPalindromes.isEmpty()) {
+            printNone();
+        }
+        else {
+            printAllPalindromes(allPalindromes);
+        }
 
-		in.close();
-	}
+        in.close();
+    }
 
-	static ArrayList<String> getAllPartsOfString(String string) {
-		ArrayList<String> allParts = new ArrayList<>();
-		int length = string.length();
+    static ArrayList<String> getAllPartsOfString(String string) {
+        ArrayList<String> allParts = new ArrayList<>();
+        int length = string.length();
 
-		String firstPart = null;
-		String secondPart = null;
+        String firstPart = null;
+        String secondPart = null;
 
-		for (int i = 1; i <= length; i++) {
-			firstPart = string.substring(0, i);
-			secondPart = string.substring(i, length);
+        for (int i = 1; i <= length; i++) {
+            firstPart = string.substring(0, i);
+            secondPart = string.substring(i, length);
 
-			allParts.add(secondPart + firstPart);
-		}
+            allParts.add(secondPart + firstPart);
+        }
 
-		return allParts;
-	}
+        return allParts;
+    }
 
-	static boolean checkIsPalindrome(String string) {
-		int length = string.length();
+    static boolean checkIsPalindrome(String string) {
+        int length = string.length();
 
-		for (int i = 0; i < length; i++) {
-			if (string.charAt(i) != string.charAt(length - 1 - i)) {
-				return false;
-			}
-		}
+        for (int i = 0; i < length; i++) {
+            if (string.charAt(i) != string.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	static void printNone() {
-		System.out.println("NONE");
-	}
+    static void printNone() {
+        System.out.println("NONE");
+    }
 
-	static void printAllPalindromes(ArrayList<String> palindromes) {
-		for (int i = 0; i < palindromes.size(); i++) {
-			System.out.println(palindromes.get(i));
-		}
-	}
+    static void printAllPalindromes(ArrayList<String> palindromes) {
+        for (int i = 0; i < palindromes.size(); i++) {
+            System.out.println(palindromes.get(i));
+        }
+    }
 }
