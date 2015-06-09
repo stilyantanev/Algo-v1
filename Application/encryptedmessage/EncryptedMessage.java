cea6190378730664581405579f5bae75e40cbd91
stilyantanev <stilyan.tanev@gmail.com>
2015-06-09 12:33:45 +0300

    Fix indentation

--- Application/encryptedmessage/EncryptedMessage.java
+++ Application/encryptedmessage/EncryptedMessage.java
@@ -3,144 +3,144 @@ package encryptedmessage;
 import java.util.Scanner;
 
 public class EncryptedMessage {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String input = in.nextLine();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
 
-		String reversedString = reverseString(input);
+        String reversedString = reverseString(input);
 
-		// First part
-		String alphabet = getAlphabet(reversedString);
+        // First part
+        String alphabet = getAlphabet(reversedString);
 
-		// Second part
-		String key = getKey(reversedString);
+        // Second part
+        String key = getKey(reversedString);
 
-		// Middle part
-		String encryptedMessage = getEncryptedMessage(reversedString);
+        // Middle part
+        String encryptedMessage = getEncryptedMessage(reversedString);
 
-		// Make whole key
-		String wholeKey = getWholeKey(key, encryptedMessage.length());
+        // Make whole key
+        String wholeKey = getWholeKey(key, encryptedMessage.length());
 
-		// Get indexes in alphabet of encrypted message
-		int[] indexesEncryptedMessage = getIndexesOfMessage(encryptedMessage, alphabet);
+        // Get indexes in alphabet of encrypted message
+        int[] indexesEncryptedMessage = getIndexesOfMessage(encryptedMessage, alphabet);
 
-		// Get indexes in alphabet of real word
-		int[] indexesRealWord = getIndexesOfRealWord(indexesEncryptedMessage, wholeKey, alphabet);
+        // Get indexes in alphabet of real word
+        int[] indexesRealWord = getIndexesOfRealWord(indexesEncryptedMessage, wholeKey, alphabet);
 
-		// Make real word
-		String realWord = makeRealWord(indexesRealWord, alphabet);
+        // Make real word
+        String realWord = makeRealWord(indexesRealWord, alphabet);
 
-		System.out.println(realWord);
+        System.out.println(realWord);
 
-		in.close();
-	}
+        in.close();
+    }
 
-	static String reverseString(String string) {
-		int length = string.length();
-		String firstHalf = string.substring(0, length / 2);
-		String secondHalf = string.substring(length / 2, length);
+    static String reverseString(String string) {
+        int length = string.length();
+        String firstHalf = string.substring(0, length / 2);
+        String secondHalf = string.substring(length / 2, length);
 
-		return secondHalf + firstHalf;
-	}
+        return secondHalf + firstHalf;
+    }
 
-	static int getLengthOfAlphabet(String string) {
-		int firstTilda = string.indexOf('~');
-		int lengthOfAlphabet = Integer.parseInt(string.substring(0, firstTilda));
+    static int getLengthOfAlphabet(String string) {
+        int firstTilda = string.indexOf('~');
+        int lengthOfAlphabet = Integer.parseInt(string.substring(0, firstTilda));
 
-		return lengthOfAlphabet;
-	}
+        return lengthOfAlphabet;
+    }
 
-	static String getAlphabet(String string) {
-		int firstTilda = string.indexOf('~');
-		String alphabet = string.substring(firstTilda + 1, firstTilda + getLengthOfAlphabet(string) + 1);
+    static String getAlphabet(String string) {
+        int firstTilda = string.indexOf('~');
+        String alphabet = string.substring(firstTilda + 1, firstTilda + getLengthOfAlphabet(string) + 1);
 
-		return alphabet;
-	}
+        return alphabet;
+    }
 
-	static int getLengthOfKey(String string) {
-		int firstTilda = string.indexOf('~');
-		int secondTilda = string.indexOf('~', firstTilda + 1);
-		int lengthOfKey = Integer.parseInt(string.substring(secondTilda + 1));
+    static int getLengthOfKey(String string) {
+        int firstTilda = string.indexOf('~');
+        int secondTilda = string.indexOf('~', firstTilda + 1);
+        int lengthOfKey = Integer.parseInt(string.substring(secondTilda + 1));
 
-		return lengthOfKey;
-	}
+        return lengthOfKey;
+    }
 
-	static String getKey(String string) {
-		int firstTilda = string.indexOf('~');
-		int secondTilda = string.indexOf('~', firstTilda + 1);
-		String key = string.substring(secondTilda - getLengthOfKey(string), secondTilda);
+    static String getKey(String string) {
+        int firstTilda = string.indexOf('~');
+        int secondTilda = string.indexOf('~', firstTilda + 1);
+        String key = string.substring(secondTilda - getLengthOfKey(string), secondTilda);
 
-		return key;
-	}
+        return key;
+    }
 
-	static String getEncryptedMessage(String string) {
-		int firstTilda = string.indexOf('~');
-		int secondTilda = string.indexOf('~', firstTilda + 1);
-		String encryptedMessage = string.substring(firstTilda + getLengthOfAlphabet(string) + 1, secondTilda - getLengthOfKey(string));
+    static String getEncryptedMessage(String string) {
+        int firstTilda = string.indexOf('~');
+        int secondTilda = string.indexOf('~', firstTilda + 1);
+        String encryptedMessage = string.substring(firstTilda + getLengthOfAlphabet(string) + 1, secondTilda - getLengthOfKey(string));
 
-		return encryptedMessage;
-	}
+        return encryptedMessage;
+    }
 
-	static String getWholeKey(String key, int length) {
-		String wholeKey = "";
-		int keyLength = key.length();
-		int lengthOfWholeKey = length;
+    static String getWholeKey(String key, int length) {
+        String wholeKey = "";
+        int keyLength = key.length();
+        int lengthOfWholeKey = length;
 
-		while (lengthOfWholeKey > 0) {
-			if (lengthOfWholeKey < keyLength) {
-				String partKey = key.substring(0, lengthOfWholeKey);
-				wholeKey += partKey;
-				break;
-			}
+        while (lengthOfWholeKey > 0) {
+            if (lengthOfWholeKey < keyLength) {
+                String partKey = key.substring(0, lengthOfWholeKey);
+                wholeKey += partKey;
+                break;
+            }
 
-			wholeKey += key;
-			lengthOfWholeKey -= keyLength;
-		}
+            wholeKey += key;
+            lengthOfWholeKey -= keyLength;
+        }
 
-		return wholeKey;
-	}
+        return wholeKey;
+    }
 
-	static int[] getIndexesOfMessage(String encryptedMessage, String alphabet) {
-		int[] indexesEncryptedMessage = new int[encryptedMessage.length()];
+    static int[] getIndexesOfMessage(String encryptedMessage, String alphabet) {
+        int[] indexesEncryptedMessage = new int[encryptedMessage.length()];
 
-		for (int i = 0; i < encryptedMessage.length(); i++) {
-			char letter = encryptedMessage.charAt(i);
+        for (int i = 0; i < encryptedMessage.length(); i++) {
+            char letter = encryptedMessage.charAt(i);
 
-			for (int j = 0; j < alphabet.length(); j++) {
-				if (letter == alphabet.charAt(j)) {
-					indexesEncryptedMessage[i] = j;
-				}
-			}
-		}
+            for (int j = 0; j < alphabet.length(); j++) {
+                if (letter == alphabet.charAt(j)) {
+                    indexesEncryptedMessage[i] = j;
+                }
+            }
+        }
 
-		return indexesEncryptedMessage;
-	}
+        return indexesEncryptedMessage;
+    }
 
-	static int[] getIndexesOfRealWord(int[] indexesEncryptedMessage, String wholeKey, String alphabet) {
-		int[] indexesRealWord = new int[indexesEncryptedMessage.length];
+    static int[] getIndexesOfRealWord(int[] indexesEncryptedMessage, String wholeKey, String alphabet) {
+        int[] indexesRealWord = new int[indexesEncryptedMessage.length];
 
-		for (int i = 0; i < indexesEncryptedMessage.length; i++) {
-			char letter = wholeKey.charAt(i);
-			int alphabetIndex = alphabet.indexOf(letter);
+        for (int i = 0; i < indexesEncryptedMessage.length; i++) {
+            char letter = wholeKey.charAt(i);
+            int alphabetIndex = alphabet.indexOf(letter);
 
-			int index = indexesEncryptedMessage[i] + alphabet.length();
-			index = index - alphabetIndex;
-			index = index % alphabet.length();
+            int index = indexesEncryptedMessage[i] + alphabet.length();
+            index = index - alphabetIndex;
+            index = index % alphabet.length();
 
-			indexesRealWord[i] = index;
-		}
+            indexesRealWord[i] = index;
+        }
 
-		return indexesRealWord;
-	}
+        return indexesRealWord;
+    }
 
-	static String makeRealWord(int[] indexesRealWord, String alphabet) {
-		String realWord = "";
+    static String makeRealWord(int[] indexesRealWord, String alphabet) {
+        String realWord = "";
 
-		for (int i = 0; i < indexesRealWord.length; i++) {
-			char letter = alphabet.charAt(indexesRealWord[i]);
-			realWord += letter;
-		}
+        for (int i = 0; i < indexesRealWord.length; i++) {
+            char letter = alphabet.charAt(indexesRealWord[i]);
+            realWord += letter;
+        }
 
-		return realWord;
-	}
+        return realWord;
+    }
 }
